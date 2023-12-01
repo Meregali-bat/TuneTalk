7944743e94a3cdf34ce977867614be99629d4b65
Lucas Meregali <lucas.smeregalli@gmail.com>
2023-12-01 00:23:11 -0300

    fix- notificações 🔔

--- models/modelComentario.js
+++ models/modelComentario.js
@@ -16,11 +16,11 @@ class Comentario {
       VALUES 
       ('${this.texto}', '${this.likes}', '${this.post_idpost}', '${this.usuario_idusuario}')
     `);
-
+  
     const post = await db.query(`SELECT * FROM post WHERE idpost = '${this.post_idpost}'`);
     const autorId = post[0].usuario_idusuario;
-
-  await db.query(`INSERT INTO notificacoes (usuario_idusuario, usuario_idusuario1, tipo, conteudo, data, lida) VALUES ('${autorId}', '${this.usuario_idusuario}', 'comment', 'comentou no seu post', CURRENT_TIMESTAMP, 0)`);
+  
+    await db.query(`INSERT INTO notificacoes (usuario_idusuario, usuario_idusuario1, tipo, conteudo, data, lida, post_id) VALUES ('${autorId}', '${this.usuario_idusuario}', 'comment', 'comentou no seu post', CURRENT_TIMESTAMP, 0, '${this.post_idpost}')`);
     
     return comentario;
   }
